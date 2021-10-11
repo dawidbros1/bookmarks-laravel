139a00b4c8a89f9ae8da0f376d600f63991a7bb1
dawidbros1 <dawidbros1@wp.pl>
2021-10-11 10:51:13 +0200

    Poprawka

--- app/Policies/PagePolicy.php
+++ app/Policies/PagePolicy.php
@@ -29,20 +29,18 @@ class PagePolicy
         return  $this->checkParent($user, new Page(), $page->type, $page->parent_id);
     }
 
-    // Tutaj strona jeszcze nie istnieje
+    // Tutaj strona jeszcze nie musi istnieje, ale może
     public function checkParent(User $user, Page $page, $type = null, $parent_id = null)
     {
         if ($type == 'subcategory') {
             $subcategory = $this->subcategory->find($parent_id);
-            $this->checkSubcategory($user, $subcategory);
+            return $this->checkSubcategory($user, $subcategory);
         } elseif ($type == 'category') {
             $category = $this->category->find($parent_id);
-            $this->checkCategory($user, $category);
+            return $this->checkCategory($user, $category);
         } else {
             return Response::deny('Nieprawidłowy typ strony');
         }
-
-        return Response::allow();
     }
 
     public function categoryAuthor($user, Page $page,  Category $category)
@@ -57,12 +55,13 @@ class PagePolicy
     {
         if ($subcategory == null) return Response::deny('Zasób nie istnieje');
         $category = $this->category->find($subcategory->category_id);
-        $this->checkCategory($user, $category);
+        return $this->checkCategory($user, $category);
     }
 
     private function checkCategory($user, Category $category)
     {
         if ($category == null) return Response::deny('Zasób nie istnieje');
         if ($category->user_id != $user->id) return Response::deny('Brak uprawnień do tego zasobu');
+        return Response::allow();
     }
 }
