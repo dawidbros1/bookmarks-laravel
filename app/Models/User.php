141b320265311eda221b478f90724223b9441989
dawidbros1 <dawidbros1@wp.pl>
2022-01-11 13:04:35 +0100

    poprawka

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use App\Repository\CategoryRepository;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
