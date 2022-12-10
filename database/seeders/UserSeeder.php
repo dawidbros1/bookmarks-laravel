1ecf06292738d319e3cbe2b739f029a1487be7bb
dawidbros1 <dawidbros1@wp.pl>
2022-12-10 17:12:21 +0100

    show password in User Seeder for created users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,13 +22,13 @@ class UserSeeder extends Seeder
                     'id' => 1,
                     'name' => "Lucky2007",
                     'email' => 'user01@wp.pl',
-                    'password' => '$2y$10$3b6H/9J2bspYVAYDMSp6je/fU.xmHQktomwrQ5JLkRepk2cXTqGvK',
+                    'password' => '$2y$10$3b6H/9J2bspYVAYDMSp6je/fU.xmHQktomwrQ5JLkRepk2cXTqGvK', // admin123
                 ],
                 [
                     'id' => 2,
                     'name' => "Olivia",
                     'email' => 'user02@wp.pl',
-                    'password' => '$2y$10$3b6H/9J2bspYVAYDMSp6je/fU.xmHQktomwrQ5JLkRepk2cXTqGvK',
+                    'password' => '$2y$10$3b6H/9J2bspYVAYDMSp6je/fU.xmHQktomwrQ5JLkRepk2cXTqGvK', // admin123
                 ],
             ],
         );
